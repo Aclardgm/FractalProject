a1834a296f25bc152768515920722bde2482773b
Aclardgm <65665567+Aclardgm@users.noreply.github.com>
2021-10-24 17:55:32 +0200

    Add files via upload

---
+++ Assets/FractaleManager.cs
@@ -0,0 +1,197 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FractaleManager : MonoBehaviour
+{
+    Material fractale;
+
+    float Zoom = 1000.0f;
+    float IterationMax = 10;
+
+    //-2.1 -> 0.6
+    float OffsetX = 0f;
+    //-1.2 -> 1.2
+    float OffsetY = 0.5f;
+    float OffsetR = 0.5f;
+    float OffsetG = 0.5f;
+    float OffsetB = 0.5f;
+    float RotAngle = 0;
+    float OffSetComplexA = 0;
+    float OffSetComplexB = 0;
+    float PowCount = 0;
+
+
+    float targetOffsetX = 0.5f;
+    float targetOffsetY = 0.5f;
+    float targetZoom = 1;
+    float targetIteratioMax = 10;
+    float targetOffsetR = 0.5f;
+    float targetOffsetG= 0.5f;
+    float targetOffsetB= 0.5f;
+    float targetRotAngle= 0;
+    float targetOffsetComplexA = 0;
+    float targetOffsetComplexB = 0;
+    float targetPowCount = 0;
+
+    float startAnim;
+    float timeAnim = 5f;
+    
+
+    public Text IterationCountText;
+
+
+    bool StopMove = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fractale = GetComponent<Image>().material;
+        StartCoroutine("Animate");
+        IterationCountText.text = ((int)targetIteratioMax).ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetValues();
+    }
+
+    public void AddIteration()
+    {
+        targetIteratioMax++;
+        IterationCountText.text = ((int)targetIteratioMax).ToString();
+    }
+
+    public void ReduceIteration()
+    {
+        if(targetIteratioMax-1 != 1)
+        {
+            targetIteratioMax--;
+            IterationCountText.text = ((int)targetIteratioMax).ToString();
+        }
+    }
+
+    public void ToggleMove()
+    {
+        StopMove = !StopMove;
+    }
+
+    private IEnumerator Animate()
+    {
+        yield return new WaitForSeconds(timeAnim);
+        startAnim = Time.time;
+        OffsetX = targetOffsetX;
+        OffsetY = targetOffsetY;
+        OffsetR = targetOffsetR;
+        OffsetG = targetOffsetG;
+        OffsetB = targetOffsetB;
+        Zoom = targetZoom;
+        RotAngle = targetRotAngle;
+        OffSetComplexA = targetOffsetComplexA;
+        OffSetComplexB = targetOffsetComplexB;
+        IterationMax = targetIteratioMax;
+        PowCount = targetPowCount;
+
+        if (!StopMove)
+        {
+            if ((int)(Random.value * 2) % 2 == 0)
+            {
+                targetOffsetX = Random.Range(-1.3f, 0.3f) + 1.5f;
+            }
+            if ((int)(Random.value * 2) % 2 == 0)
+            {
+                targetOffsetY = Random.Range(-0.8f, 0.8f) + 0.5f;
+            }
+            targetZoom = Random.value * Mathf.Pow(10000, Random.Range(1, 2));// ;
+            targetRotAngle = Random.value * 360;
+        }
+
+        if ((int)(Random.value * 2) % 2 == 0)
+        {
+            targetOffsetComplexA = Random.Range(-1.5f, 1.5f);
+        }
+        if ((int)(Random.value * 2) % 2 == 0)
+        {
+            targetOffsetComplexB = Random.Range(-1.5f, 1.5f);
+        }
+
+
+        //if ((int)(Random.value * 2) % 2 == 0)
+        //{
+        //    targetPowCount = Random.Range(2, 2);
+        //}
+        targetPowCount = 2;
+
+
+        //if ((int)(Random.value*2) % 2 == 0)
+        //{
+        //    targetIteratioMax = Random.Range(10, 100);
+        //}
+        if ((int)(Random.value * 2) % 2 == 0)
+        {
+            targetOffsetR = Random.Range(0.0f, 1.0f);
+        }
+        if ((int)(Random.value * 2) % 2 == 0)
+        {
+            targetOffsetG = Random.Range(0.0f, 1.0f);
+        }
+        if ((int)(Random.value * 2) % 2 == 0)
+        {
+            targetOffsetB = Random.Range(0.0f, 1.0f);
+        }
+        timeAnim = Random.Range(5.0f, 8.0f);
+        StartCoroutine("Animate");
+    }
+
+    void SetValues()
+    {
+
+        float x = Mathf.Lerp(OffsetX, targetOffsetX, (Time.time - startAnim) / timeAnim);
+        float y = Mathf.Lerp(OffsetY, targetOffsetY, (Time.time - startAnim) / timeAnim);
+        float r = Mathf.Lerp(OffsetR, targetOffsetR, (Time.time - startAnim) / timeAnim);
+        float g = Mathf.Lerp(OffsetG, targetOffsetG, (Time.time - startAnim) / timeAnim);
+        float b = Mathf.Lerp(OffsetB, targetOffsetB, (Time.time - startAnim) / timeAnim);
+        float z = Mathf.Lerp(Zoom, targetZoom, (Time.time - startAnim) / timeAnim);
+        float i = (int)Mathf.Lerp(IterationMax, targetIteratioMax, (Time.time - startAnim) / timeAnim);
+        float rot = Mathf.Lerp(RotAngle, targetRotAngle, (Time.time - startAnim) / timeAnim) * Mathf.Deg2Rad;
+
+
+        float offCompA = Mathf.Lerp(OffSetComplexA, targetOffsetComplexA, (Time.time - startAnim) / timeAnim);
+        float offCompB = Mathf.Lerp(OffSetComplexB, targetOffsetComplexB, (Time.time - startAnim) / timeAnim);
+
+
+        float powC = Mathf.Lerp(PowCount, targetPowCount, (Time.time - startAnim) / timeAnim);
+
+        //x = 0.6f;
+        //y = 0;
+
+        if(!StopMove)
+        {
+            fractale.SetFloat("_OffsetX", x);
+            fractale.SetFloat("_OffsetY", y);
+            fractale.SetFloat("_Zoom", z);
+            //fractale.SetFloat("_RotAngle", rot);
+        }
+
+        fractale.SetFloat("_OffsetR", r);
+        fractale.SetFloat("_OffsetG", g);
+        fractale.SetFloat("_OffsetB", b);
+        fractale.SetFloat("_IterationMax", targetIteratioMax);
+
+
+        if (fractale.HasProperty("_OffSetComplexA") && fractale.HasProperty("_OffSetComplexB"))
+        {
+            fractale.SetFloat("_OffSetComplexA", offCompA);
+            fractale.SetFloat("_OffSetComplexB", offCompB);
+        }
+
+
+        if (fractale.HasProperty("_PowCount"))
+        {
+            fractale.SetFloat("_PowCount", (int)powC);
+        }
+
+    }
+}
---
+++ Assets/FractaleShader.shader
@@ -0,0 +1,125 @@
+﻿Shader "Hidden/FractaleShader"
+{
+    Properties
+    {
+		_Zoom("Zoom",float) = 10000.0
+		_IterationMax("IterationMax",float) = 1000
+		_OffsetX("OffsetX",float) = 0
+		_OffsetY("OffsetY",float) = 0
+		_OffsetR("OffsetR",float) = 0
+		_OffsetG("OffsetG",float) = 0
+		_OffsetB("OffsetB",float) = 0
+
+		_SizeImageX("SizeImageX",float) = 0
+		_SizeImageY("SizeImageY",float) = 0
+		_RotAngle("RotAngle",float) = 0
+		_OffSetComplexA("OffSetComplexA",float) = 0
+		_OffSetComplexB("OffSetComplexB",float) = 0
+		_PowCount("PowCount",float) = 2
+    }
+    SubShader
+    {
+		Tags{
+			"RenderType" = "Transparent"
+			"Queue" = "Transparent"
+		}
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+
+			float _Zoom;
+			float _IterationMax;
+			float _OffsetX;
+			float _OffsetY;
+
+			float _OffsetR;
+			float _OffsetG;
+			float _OffsetB;
+
+			float _SizeImageX;
+			float _SizeImageY;
+
+			float _RotAngle;
+
+			float _OffSetComplexA;
+			float _OffSetComplexB;
+
+			float _PowCount;
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+				fixed4 col = (0,0,0,0);
+
+
+				float x1 = i.uv.x - _OffsetX;
+				float x2 = i.uv.x + _OffsetX;
+				float y1 = i.uv.y - _OffsetY;
+				float y2 = i.uv.y + _OffsetY;
+
+				x1 = x1 * cos(_RotAngle) + y1 * sin(_RotAngle);
+				y1 = y1 * cos(_RotAngle) - x1 * sin(_RotAngle);
+
+
+				x2 = x2 * cos(_RotAngle) + y2 * sin(_RotAngle);
+				y2 = y2 * cos(_RotAngle) - x2 * sin(_RotAngle);
+
+
+
+				float c_r = 0.285 + _OffSetComplexA;// (rotUVx) / _Zoom + x1;
+				float c_i = 0.01 + _OffSetComplexB;// (rotUVy) / _Zoom + y1;
+				float z_r = (i.uv.x * _SizeImageX) / _Zoom + x1;
+				float z_i = (i.uv.y * _SizeImageY) / _Zoom + y1;
+				int iter = 0;
+				//Julia animated deg 3
+				for (iter = 0; iter < _IterationMax && z_r*z_r + z_i * z_i < 4; iter++) {
+					float tmp = z_r;
+					z_r = z_r * z_r *z_r - 3 * z_i * z_i * z_r + c_r;
+					z_i = 3 * tmp * tmp * z_i  - z_i * z_i * z_i + c_i;
+				}
+				if (iter == _IterationMax)
+				{
+					col.rgb = half3(0, 0, 0);
+				}
+				else
+				{
+					float r = (((iter / _IterationMax) + _OffsetR) * 1000) % 1000 / 1000.0f;
+					float g = (((iter / _IterationMax) + _OffsetG) * 1000) % 1000 / 1000.0f;
+					float b = (((iter / _IterationMax) + _OffsetB) * 1000) % 1000 / 1000.0f;
+
+					col.rgb = half3(r,g,b);
+				}
+
+				return col;
+            }
+            ENDCG
+        }
+    }
+}
